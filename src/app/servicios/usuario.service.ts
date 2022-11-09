110642378d22b996845d6592307fdecc37716f71
lrloachamin <luisloachamin1128@gmail.com>
2022-11-08 20:42:21 -0500

    Mejora Registro usuario

--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -12,8 +12,8 @@ export class UsuarioService {
   }
 
   obtenerUsuario(){
-   // const url='https://gpr-espe.azurewebsites.net/api/v1/usuario';
-    const url= 'http://localhost:8080/api/v1/usuario';
+   const url='https://gpr-espe.azurewebsites.net/api/v1/usuario';
+   // const url= 'http://localhost:8080/api/v1/usuario';
     return this.http.get(url); 
   }
 
